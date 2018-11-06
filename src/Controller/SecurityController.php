6125e7a167ed69e0aabf5856ea637652cf0c5c4c
Lucas <toonerad@hotmail.com>
2018-11-06 15:04:50 +0100

    Change form type and create register page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
@@ -13,11 +15,18 @@ class SecurityController extends AbstractController
     /**
      * @Route(path="/register", name="security_registration")
      */
-    public function registration()
+    public function registration(Request $request, ObjectManager $manager)
     {
         $user = new User();
 
         $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($user);
+            $manager->flush();
+        }
 
         return $this->render('security/register.html.twig', [
             'form' => $form->createView()
