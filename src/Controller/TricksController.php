81abc2a79148558072910e741440558b7c3119e7
Lucas <toonerad@hotmail.com>
2018-11-07 20:25:15 +0100

    Add tricks to view

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +13,12 @@ class TricksController extends AbstractController
      */
     public function index()
     {
+        $repo = $this->getDoctrine()->getRepository(Trick::class);
+
+        $tricks = $repo->findAll();
+
         return $this->render('tricks/index.html.twig', [
-            'controller_name' => 'TricksController',
+            'tricks' => $tricks
         ]);
     }
 }
